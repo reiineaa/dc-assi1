cc2b284c68521ba04d04be23be61cb4002c3dd72
reiineaa <80281865+reiineaa@users.noreply.github.com>
2022-10-30 19:39:57 +0600

    2

--- examples/WebServer/src/main/java/org/example/Processor.java
+++ examples/WebServer/src/main/java/org/example/Processor.java
@@ -1,8 +1,7 @@
 package org.example;
 
-import java.io.IOException;
-        import java.io.PrintWriter;
-        import java.net.Socket;
+import java.io.*;
+import java.net.Socket;
 
 /**
  * Processor of HTTP request.
@@ -10,19 +9,101 @@ import java.io.IOException;
 public class Processor {
     private final Socket socket;
     private final HttpRequest request;
+    public String response = "";
 
     public Processor(Socket socket, HttpRequest request) {
         this.socket = socket;
         this.request = request;
     }
 
+
     public void process() throws IOException {
-        // Print request that we received.
         System.out.println("Got request:");
-        System.out.println(request);
+        System.out.println(request.toString());
         System.out.flush();
 
-        // To send response back to the client.
+        String s = request.toString();
+        int pos;
+        char c;
+        String name = "";
+        if(s.contains("create")) {
+
+            for(pos = 12; s.charAt(pos) != ' '; ++pos) {
+                c = s.charAt(pos);
+                name = name + c;
+            }
+
+            File myObj = new File(name);
+            if (myObj.createNewFile()) {
+                String var10001 = this.response;
+                this.response = var10001 + "File created: " + myObj.getName();
+            } else {
+                this.response = this.response + "File already exists.";
+            }
+
+        }
+        else if(s.contains("delete")) {
+            for(pos = 12; s.charAt(pos) != ' '; ++pos) {
+                c = s.charAt(pos);
+                name = name + c;
+            }
+            File obj = new File(name);
+            if (obj.delete()) {
+                String var10001 = this.response;
+                this.response = var10001 + "File deleted: " + obj.getName();
+            } else {
+                this.response = this.response + "File successfully delete";
+            }
+        }
+        else if(s.contains("write")){
+            pos = 11;
+            while(s.charAt(pos) != '/'){
+                c = s.charAt(pos);
+                name += c;
+                pos++;
+            }
+//            FileWriter myWriter = new FileWriter(name);
+//            myWriter.write("some text");
+//            myWriter.close();
+            PrintWriter writer = new PrintWriter("text", "UTF-8");
+            writer.println("The United Nations Educational, Scientific and Cultural Organization[a] is a specialised agency of the United Nations (UN) aimed at promoting world peace and security through international cooperation in education, arts, sciences and culture.\n"+
+                    "It has 193 member states and 12 associate members, as well as partners in the non-governmental, intergovernmental and private sector."+
+                    "Headquartered at the World Heritage Centre in Paris, France, UNESCO has 53 regional field offices and 199 national commissions that facilitate its global mandate.");
+            writer.close();
+            this.response += "Successfully wrote to the file.";
+        }
+        else if(s.contains("execute")) {
+            for (pos = 13; s.charAt(pos) != ' '; ++pos) {
+                c = s.charAt(pos);
+                name = name + c;
+            }
+            String ch="a";
+            int count=0;
+            BufferedReader myReader = new BufferedReader(new FileReader(name));
+            String strCurrentLine;
+            while ((strCurrentLine = myReader.readLine()) != null) {
+                String[] words = strCurrentLine.split("");
+                for (String word : words) {
+                    if (word.equals(ch))
+                    {
+                        count++;
+                    }
+                }
+                this.response += strCurrentLine;
+            }
+            if(count!=0)
+            {
+                this.response = "The written character is repeated for "+count+ " times in the given file";
+            }
+            else
+            {
+                this.response = "The written character is not present in the given file";
+            }
+            myReader.close();
+        }
+
+
+
         PrintWriter output = new PrintWriter(socket.getOutputStream());
 
         // We are returning a simple web page now.
@@ -31,29 +112,10 @@ public class Processor {
         output.println();
         output.println("<html>");
         output.println("<head><title>Hello</title></head>");
-        if (request.getRequestLine().equals("GET /create/itemid HTTP/1.1")) {
-            output.println("<body><p>itemid has been created!</p></body>");
-        } else if (request.getRequestLine().equals("GET /delete/itemid HTTP/1.1")) {
-            output.println("<body><p>itemid has been deleted!</p></body>");
-        } else if (request.getRequestLine().equals("GET /exec/params HTTP/1.1")) {
-            output.println("<body><p>Parameters has been executed!</p></body>");
-        } else {
-            String req = request.getRequestLine();
-            req = req.replace("GET /", "").replace(" HTTP/1.1", "");
-            String[] reqs = req.split("/");
-            System.out.println(reqs);
-            if (reqs[0].equals("calculate") && reqs[1].equals("factorial")) {
-                double f = 1;
-                for (int i = 1; i <= Integer.parseInt(reqs[2]); i++) {
-                    f *= i;
-                }
-                output.println("<body><p>Factorial of " + reqs[2] + " is " + f + "</p></body>");
-            }
-        }
-        output.println("<body><p>Hello, world!</p></body>");
+        output.println("<body><p>" + this.response + "</p></body>");
         output.println("</html>");
         output.flush();
-
         socket.close();
+        this.response = "";
     }
 }
